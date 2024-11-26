ca9ff02e6f45964d418e2ef01615a973792e79cd
Piotr Szuflicki <piotr.szuflicki@goelett.com>
2024-11-26 11:15:23 +0100

    Initial commit

---
+++ FileSearchEngine/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace FileSearchEngine.Models;
+
+public class ErrorViewModel {
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
